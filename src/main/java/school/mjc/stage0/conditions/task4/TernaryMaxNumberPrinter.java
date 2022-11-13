03183495107971158254aa6eaf674b306f285363
NaziraDi <diushebekova@gmail.com>
2022-11-13 22:51:17 +0600

    task4 is done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        int result;
+        result = first > second ? first : second;
+        result = third > result ? third : result;
+
+        System.out.println(result);
     }
 }
